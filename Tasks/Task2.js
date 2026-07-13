7595148441f2c45ad37ccd203798ccbd633c84c0
Favour Onyejekwe <160279539+Cataleabonez@users.noreply.github.com>
2026-07-13 14:40:55 +0100

    fix

--- Tasks/Task2.js
+++ Tasks/Task2.js
@@ -1,1 +1,5 @@
-
+/*I find it very facinating that javascript was invented in just 10 days! 
+what kind of dedication went into it.
+Oh wahat a way to discover that Java and Javascript are two worlds apart. I am glad to have this knowledge. 
+Javascript is a versatile and is a default for our web browsers. 
+Javascript is litrally everywhere, from front-end to back-end development. */
---
+++ Tasks/Task5.js
@@ -0,0 +1,4 @@
+const day = Thursday;
+const position = 5;
+
+const output = 'Today is '+ day +' and it is the ' + position + 'th day of the week.';
